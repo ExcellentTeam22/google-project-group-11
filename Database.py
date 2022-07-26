79d721112777cf0132ad2b8a51a5d05c8e2633fa
eindor <eindor13@gmail.com>
2022-07-26 10:36:03 +0300

    added Database

--- Database.py
+++ Database.py
@@ -1,14 +1,15 @@
 import os
 
-def ReadData():
-    wordDict = {}
+
+def get_data():
+    word_dict = {}
     for root, dirs, files in os.walk('resources'):
         for file in files:  #get each file within the directory and subdirectories
             path = (os.path.abspath(os.path.join(root, file)))  #get the full path of each file
             with open(path, encoding="utf8") as f:  #open the file
                 for line in f:
                     for word in line.split(" "):    #every word seperated by space
-                        if word not in wordDict.keys():
-                            wordDict[word] = []
-                        wordDict[word].append(hash(line)) #add the line to the key word
-    print(wordDict)git
+                        if word not in word_dict.keys():
+                            word_dict[word] = []
+                        word_dict[word].append(hash(line)) #add the line to the key word
+    return word_dict
