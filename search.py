176fe2043818d8ec71e02e73a56e692a6ad5a471
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-07-26 17:06:25 +0300

    sentences list

--- search.py
+++ search.py
@@ -2,28 +2,36 @@ import os
 from dataclasses import dataclass
 
 
+class DB:
+    def __init__(self):
+        self.words_dict = get_data()
+
+
 def get_data() -> dict[set[str]]:
     """
-    get_data function read all the files in the Resources,
-     inside all directory and
-    subdirectories.
-    :return:A dictionary that each key is a word inside a file,
-     and for each word a list of
-    lines that contain that word (the line is converted to hash)
+    Read all the files from the Resources.
+    :return:A dictionary which each key is a word inside a file,
+    and for each word a list of number of lines where contain this word.
     """
     word_dict = {}
-    for root, dirs, files in os.walk('resources'):
-        for file in files:  #get each file within the directory and subdirectories
-            path = (os.path.abspath(os.path.join(root, file)))  #get the full path of each file
-            with open(path, encoding="utf8") as f:  #open the file
+    sentence_list = list(list())
+    for root, dirs, files in os.walk('Archive'):
+        for file in files:  # get each file within the directory and subdirectories
+            path = (os.path.abspath(os.path.join(root, file)))  # get the full path of each file
+            with open(path, encoding="utf8") as f:  # open the file
+                line_num = 0
                 for line in f:
-                    for word in line.split(" "):    #every word seperated by space
+                    line_num += 1
+                    sentence_list.append([line, file[:-4], line_num])
+                    for word in line.split(" "):  # every word separated by space
                         word = word.lower()
                         if word not in word_dict.keys():
                             word_dict[word] = set()
-                        word_dict[word].add(line) #add the line to the key word
+                        word_dict[word].add(line)  # add the line to the key word
+    print(sentence_list)
     return word_dict
 
+get_data()
 
 @dataclass
 class AutoCompleteData:
@@ -60,4 +68,5 @@ def print_user_input() -> None:
     for result in get_best_k_completions(user_search):
         print(result)
 
-print_user_input()
+
+#print_user_input()
