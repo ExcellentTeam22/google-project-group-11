3ea90000194611e0b99ccb3a40bfa3ef0d8013be
eindor <eindor13@gmail.com>
2022-07-26 11:06:00 +0300

    added comments for DB

--- Database.py
+++ Database.py
@@ -1,7 +1,13 @@
 import os
 
 
-def get_data():
+def get_data() -> dict[list[int]]:
+    """
+    get_data function read all the files in the Resources, inside all directory and
+    subdirectories.
+    :return: A dictionary that each key is a word inside a file, and for each word a list of
+        lines that contain that word (the line is converted to hash)
+    """
     word_dict = {}
     for root, dirs, files in os.walk('resources'):
         for file in files:  #get each file within the directory and subdirectories
